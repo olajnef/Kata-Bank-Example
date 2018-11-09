43f525d8f2764b68fd0faffd0faed4e97c6b64e1
olajnef <ons.lajnef@softeam.fr>
2018-11-09 16:01:17 +0100

    US1: j'ai ajouté des logs et le cas de test pour un montant négatif

--- src/kataBankClasses/Account.java
+++ src/kataBankClasses/Account.java
@@ -1,22 +1,24 @@
 package kataBankClasses;
 
 import java.time.LocalDate;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Account {
-	public Client client ;
-	
-	public long amount ;
-	public long balance ;
+	public Client client;
+
+	public long amount;
+	public long balance;
+
 	public enum Operation {
-		deposit,withdraw 
+		deposit, withdraw
 	}
-	public LocalDate date ;
-	
-	public  Account.Operation operationType ;
-	
-	
-	
-	
+
+	public LocalDate date;
+
+	public Account.Operation operationType;
+
+	private static final Logger LOGGER = Logger.getLogger(Account.class.getName());
 
 	/**
 	 * @param nameClient
@@ -24,98 +26,125 @@ public class Account {
 	 * @param balance
 	 * @param date
 	 */
-	public Account(Client client, long amount, long balance, LocalDate date,Account.Operation operationType) {
-		this.operationType=operationType;
+	public Account(Client client, long amount, long balance, LocalDate date, Account.Operation operationType) {
+		this.operationType = operationType;
 		this.client = new Client();
 		this.amount = amount;
 		this.balance = balance;
 		this.date = date;
 	}
+
 	/**
 	 * @return the amount
 	 */
 	public long getAmount() {
 		return amount;
 	}
+
 	/**
-	 * @param amount the amount to set
+	 * @param amount
+	 *            the amount to set
 	 */
 	public void setAmount(long amount) {
 		this.amount = amount;
 	}
+
 	/**
 	 * @return the balance
 	 */
 	public long getBalance() {
 		return balance;
 	}
+
 	/**
-	 * @param balance the balance to set
+	 * @param balance
+	 *            the balance to set
 	 */
 	public void setBalance(long balance) {
 		this.balance = balance;
 	}
+
 	/**
 	 * @return the date
 	 */
 	public LocalDate getDate() {
 		return date;
 	}
+
 	/**
-	 * @param date the date to set
+	 * @param date
+	 *            the date to set
 	 */
 	public void setDate(LocalDate date) {
 		this.date = date;
 	}
+
 	/**
 	 * @return the client
 	 */
 	public Client getClient() {
 		return client;
 	}
+
 	/**
-	 * @param client the client to set
+	 * @param client
+	 *            the client to set
 	 */
 	public void setClient(Client client) {
 		this.client = client;
 	}
-	
-	
-	
-	
+
 	/**
 	 * @return the operationType
 	 */
 	public Account.Operation getOperationType() {
 		return operationType;
 	}
+
 	/**
-	 * @param operationType the operationType to set
+	 * @param operationType
+	 *            the operationType to set
 	 */
 	public void setOperationType(Account.Operation operationType) {
 		this.operationType = operationType;
 	}
-	public long deposit(long amount,long balance ){
-		this.amount=amount ;
-		this.balance=balance;
-		if(isDeposit(this.operationType)){
-			if(amount>0)
-				balance+=amount;
-			
+
+	public long deposit(long amount, long balance) {
+		this.amount = amount;
+		this.balance = balance;
+		if (isDeposit(this.operationType)) {
+			if (isPositifDeposit(amount)) {
+				balance += amount;
+			} else if (isNegatifDeposit(amount)) {
+
+				throw new IllegalStateException("You can't deposit a negative value.You should try again");
+
+			}
 		}
-		
-		else System.out.println("the operation is not a deposit");
+
+		else {
+			LOGGER.info("the operation is not a deposit");
+
+		}
 		return balance;
 	}
-	
-	public Boolean isDeposit(Account.Operation operationType){
-		this.operationType= operationType;
-		return  Operation.deposit.equals(operationType);
+
+	public Boolean isDeposit(Account.Operation operationType) {
+		this.operationType = operationType;
+		return Operation.deposit.equals(operationType);
 	}
-	
-	public Boolean isWithDraw(Account.Operation operationType){
-		this.operationType= operationType;
+
+	public Boolean isWithDraw(Account.Operation operationType) {
+		this.operationType = operationType;
 		return Operation.withdraw.equals(operationType);
+	}
+
+	public Boolean isPositifDeposit(long amount) {
+		return amount > 0;
+	}
+
+	public Boolean isNegatifDeposit(long amount) {
+		return amount < 0;
 	}
 
 }
--- src/kataBankTest/KataBankTest.java
+++ src/kataBankTest/KataBankTest.java
@@ -12,32 +12,35 @@ import kataBankClasses.Account.Operation;
 import kataBankClasses.Client;
 
 public class KataBankTest {
-	private Account account;
+	private Account accountForDeposit;
+	private Account accountForWithDraw;
 	private Client client;
 
 	@Before
 	public void setUp() throws Exception {
 		client = new Client();
 		client.setName("Toto");
-		account = new Account(client,50,100,LocalDate.now(),Operation.deposit);
+		accountForDeposit = new Account(client,50,100,LocalDate.now(),Operation.deposit);
+		accountForWithDraw = new Account(client,50,100,LocalDate.now(),Operation.withdraw);
 
 	}
 	
 	@Test
 	public void account_operation_type_should_be_deposit_when_we_call_deposit_service(){
-		account.setOperationType(Operation.withdraw);
-		long currentBalance=account.deposit(account.getAmount(), account.getBalance());
-		assertEquals("the operation is not a deposit",currentBalance,account.getBalance());
+		
+		long currentBalance=accountForWithDraw.deposit(accountForWithDraw.getAmount(), accountForWithDraw.getBalance());
+		assertEquals("the operation is not a deposit",currentBalance,accountForWithDraw.getBalance());
 	}
 
 	@Test
 	public void should_deposit_positif_amount() {
-	long currentBalance = account.deposit(account.getAmount(),account.getBalance());
-	assertEquals(account.getAmount()+account.getBalance(), currentBalance);
+	long currentBalance = accountForDeposit.deposit(accountForDeposit.getAmount(),accountForDeposit.getBalance());
+	assertEquals(accountForDeposit.getAmount()+accountForDeposit.getBalance(), currentBalance);
 	}
 	
-	@Test
-	public void shouldNotDepositNegatifAmount(){
+	@Test(expected=IllegalStateException.class) 
+	public void should_not_deposit_negatif_amount(){
+		accountForDeposit.deposit(-50, accountForDeposit.getBalance());
 		
 	}
 	
