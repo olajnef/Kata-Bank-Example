5ebf054d65c830237be12a7d69cf610c39fb4a2a
olajnef <ons.lajnef@softeam.fr>
2018-11-12 11:41:51 +0100

    US 3 : j'ai corrigé les tests unitaires ainsi que la conception

--- src/kataBankClasses/Operation.java
+++ src/kataBankClasses/Operation.java
@@ -4,91 +4,82 @@ import java.time.LocalDate;
 import java.util.logging.Logger;
 
 public class Operation {
-	
+
 	public long amount;
-	
+
 	public LocalDate date;
+
 	public enum OperationType {
 		deposit, withdraw
 	}
+
 	public Operation.OperationType operationType;
-	public Account account ;
+	public Account account;
+	
+
+	public Operation(long amount, LocalDate date, OperationType operationType) {
+		super();
+		this.amount = amount;
+		this.date = date;
+		this.operationType = operationType;
+		
+	}
+
 	/**
 	 * @return the amount
 	 */
 	public long getAmount() {
 		return amount;
 	}
+
 	/**
-	 * @param amount the amount to set
+	 * @param amount
+	 *            the amount to set
 	 */
 	public void setAmount(long amount) {
 		this.amount = amount;
 	}
-	
+
 	/**
 	 * @return the date
 	 */
 	public LocalDate getDate() {
 		return date;
 	}
+
 	/**
-	 * @param date the date to set
+	 * @param date
+	 *            the date to set
 	 */
 	public void setDate(LocalDate date) {
 		this.date = date;
 	}
 
-	private static final Logger LOGGER = Logger.getLogger(Operation.class.getName());
-public long deposit(long amount, long balance) {
-		
-		if (isDeposit(this.operationType)) {
-			if (amount>0) {
-				balance += amount;
-			} else if (amount<0) {
+	public long deposit(long amount, long balance) {
 
-				throw new IllegalStateException("You can't deposit a negative value.You should try again");
+		if (amount > 0) {
+			balance += amount;
+		} else if (amount < 0) {
 
-			}
-		}
-
-		else {
-			
-			LOGGER.info("the operation is not a deposit");
+			throw new IllegalStateException("You can't deposit a negative value.You should try again");
 
 		}
+
 		return balance;
 	}
-	
-	
-	public long withdraw(long amount , long balance){
-		
-		if(isWithdraw(this.operationType)){
-			
-		if(isPositifAmount(amount)){
-			if(amount<balance){
-			balance-=amount;
-			}
-			else throw new IllegalStateException("You have not enough money to retrieve this amount");
-		}
-		else if(isNegatifAmount(amount)){
+
+	public long withdraw(long amount, long balance) {
+
+		if (isPositifAmount(amount)) {
+			if (amount < balance) {
+				balance -= amount;
+			} else
+				throw new IllegalStateException("You have not enough money to retrieve this amount");
+		} else if (isNegatifAmount(amount)) {
 			throw new IllegalStateException("You can't retrieve a negative value.You should try again");
 		}
-		}
-		else LOGGER.info("the operation is not a withdrawal");
+
 		return balance;
-	}
-	
-	
-
-	public Boolean isDeposit(Operation.OperationType operationType) {
-		this.operationType = operationType;
-		return OperationType.deposit.equals(operationType);
-	}
-
-	public Boolean isWithdraw(Operation.OperationType operationType) {
-		this.operationType = operationType;
-		return OperationType.withdraw.equals(operationType);
 	}
 
 	public Boolean isPositifAmount(long amount) {
@@ -100,8 +91,3 @@ public long deposit(long amount, long balance) {
 	}
 
 }
-
-	
-
-
-
--- src/kataBankTest/KataBankTest.java
+++ src/kataBankTest/KataBankTest.java
@@ -13,77 +13,52 @@ import kataBankClasses.Client;
 import kataBankClasses.Operation;
 
 public class KataBankTest {
-	private Operation accountForDeposit;
-	private Operation accountForWithdraw;
+	private Operation operationForDeposit;
+	private Operation operationForWithdraw;
+	private Account account;
 	private Client client;
 
 	@Before
 	public void setUp() throws Exception {
+		account = new Account(client, 100);
 		client = new Client();
 		client.setName("Toto");
-		accountForDeposit = new Account(client, 100, OperationType.deposit);
-		accountForWithdraw = new Account(client,  100, OperationType.withdraw);
+		operationForDeposit = new Operation( 50,LocalDate.now(), OperationType.deposit);
+		operationForWithdraw = new Operation(50,LocalDate.now(), OperationType.withdraw);
 
 	}
 
-	@Test
-	public void account_operation_type_should_be_deposit_when_we_call_deposit_service() {
-
-		long currentBalance = accountForDeposit.deposit(50,
-				accountForDeposit.getBalance());
-		assertEquals(50 + accountForDeposit.getBalance(), currentBalance);
-	}
 	
 	@Test
-	public void account_operation_type_should__not_be_withdraw_when_we_call_deposit_service() {
-
-		long currentBalance = accountForWithdraw.deposit(50,
-				accountForWithdraw.getBalance());
-		assertEquals("the operation is not a deposit", currentBalance, accountForWithdraw.getBalance());
-	}
-
-	@Test
 	public void should_deposit_positif_amount() {
-		long currentBalance = accountForDeposit.deposit(50, accountForDeposit.getBalance());
-		assertEquals(50 + accountForDeposit.getBalance(), currentBalance);
+		long currentBalance = operationForDeposit.deposit(50, account.getBalance());
+		assertEquals(50 + account.getBalance(), currentBalance);
 	}
 
 	@Test(expected = IllegalStateException.class)
 	public void should_not_deposit_negatif_amount() {
-		accountForDeposit.deposit(-50, accountForDeposit.getBalance());
+		operationForDeposit.deposit(-50, account.getBalance());
 
 	}
-	@Test
-	public void account_operation_type_should_not_be_deposit_when_we_call_withdraw_service(){
-		long currentBalance = accountForDeposit.withdraw(50,
-				accountForDeposit.getBalance());
-		assertEquals("the operation is not a withdraw", currentBalance, accountForDeposit.getBalance());
-	}
-	@Test
-	public void account_operation_type_should_be_withdraw_when_we_call_deposit_service() {
-
-		long currentBalance = accountForWithdraw.withdraw(50,
-				accountForWithdraw.getBalance());
-		assertEquals( accountForWithdraw.getBalance()-50 , currentBalance);
-	}
+	
 	@Test
 	public void should_withdraw_positif_amount(){
-		long currentBalance = accountForWithdraw.withdraw(50, accountForWithdraw.getBalance());
-		assertEquals(accountForWithdraw.getBalance()-50, currentBalance);
+		long currentBalance = operationForWithdraw.withdraw(50, account.getBalance());
+		assertEquals(account.getBalance()-50, currentBalance);
 	}
 	
 	@Test (expected = IllegalStateException.class)
 	public void should_not_withdraw_negatif_amount(){
-		accountForWithdraw.withdraw(-50, accountForWithdraw.getBalance());
+		operationForWithdraw.withdraw(-50, account.getBalance());
 	}
 	@Test
 	public void should_retrieve_positif_amount_less_than_balance(){
-		long currentBalance = accountForWithdraw.withdraw(50, accountForWithdraw.getBalance());
-		assertEquals(accountForWithdraw.getBalance()-accountForWithdraw.getAmount(), currentBalance);
+		long currentBalance = operationForWithdraw.withdraw(50, account.getBalance());
+		assertEquals(account.getBalance()-operationForWithdraw.getAmount(), currentBalance);
 		}
 	@Test (expected = IllegalStateException.class)
 	public void should_not_retrieve_positif_amount_higher_than_balance(){
-		accountForWithdraw.withdraw(150, 100);
+		operationForWithdraw.withdraw(150, account.getBalance());
 	}
 
 }
